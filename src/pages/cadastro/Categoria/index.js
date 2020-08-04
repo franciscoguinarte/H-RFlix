2287df5223a7e153cddeeb78a6ea6ffce1fc6f52
francisco <francisco.guinarte@outlook.com.br>
2020-08-03 21:24:29 -0300

    Update back end

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://hrflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
